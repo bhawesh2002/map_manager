3b99e9fa583425a84b7a9333bfcabe2f2d5509bc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-06-08 20:45:25 +0530

    Refactor exports in map_manager_mapbox.dart to include tracking_mode_class and remove ride_tracking_mode_class

--- lib/map_manager_mapbox.dart
+++ lib/map_manager_mapbox.dart
@@ -1,5 +1,5 @@
 /// A Flutter package that provides an abstraction layer over mapbox_maps_flutter.
-/// 
+///
 /// This package simplifies map interactions for developers by providing
 /// a mode-based approach to handling different map functionalities.
 library map_manager_mapbox;
@@ -14,7 +14,7 @@ export 'manager/map_mode.dart';
 export 'manager/data_classes/basic_mode_class.dart';
 export 'manager/data_classes/loc_mode_class.dart';
 export 'manager/data_classes/route_mode_class.dart';
-export 'manager/data_classes/ride_tracking_mode_class.dart';
+export 'manager/data_classes/tracking_mode_class.dart';
 
 // Export utilities
 export 'manager/location_update.dart';
@@ -22,4 +22,3 @@ export 'manager/map_utils.dart';
 export 'manager/map_exceptions.dart';
 export 'utils/list_value_notifier.dart';
 export 'utils/utils.dart';
-
