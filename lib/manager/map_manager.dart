6acc1d3c9f0b8421d9dab6e00d3db2435c3eab5b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-06-10 12:39:56 +0530

    Refactor MapManager: rename RideTrackingModeClass to TrackingModeClass and update related methods for consistency

--- lib/manager/map_manager.dart
+++ lib/manager/map_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
+import 'package:map_manager_mapbox/manager/location_update.dart';
 import 'package:map_manager_mapbox/manager/map_mode.dart';
 import 'package:map_manager_mapbox/manager/map_utils.dart';
 import 'package:map_manager_mapbox/utils/list_value_notifier.dart';
@@ -10,7 +11,6 @@ import 'data_classes/basic_mode_class.dart';
 import 'data_classes/loc_mode_class.dart';
 import 'data_classes/tracking_mode_class.dart';
 import 'data_classes/route_mode_class.dart';
-import 'location_update.dart';
 
 class MapManager extends ChangeNotifier {
   MapMode _mode;
@@ -18,7 +18,7 @@ class MapManager extends ChangeNotifier {
   late final AnimationController _animationController;
   MapManager._(this._mapboxMap, this._mode, this._animationController);
 
-  RideTrackingModeClass? _trackingModeClass;
+  TrackingModeClass? _trackingModeClass;
   BasicModeClass? _basicModeClass;
   LocationModeClass? _locationModeClass;
   RouteModeClass? _routeModeClass;
@@ -55,7 +55,7 @@ class MapManager extends ChangeNotifier {
     }, route: (routeMode) async {
       await _handleRouteMode(routeMode);
     }, tracking: (TrackingMode value) async {
-      await _handleRideTrackingMode(value);
+      await _handleTrackingMode(value);
     });
   }
 
@@ -75,6 +75,32 @@ class MapManager extends ChangeNotifier {
     });
   }
 
+  Future<void> _handleBasicMode(BasicMapMode basic) async {
+    _mode.ensureMode<BasicMapMode>();
+    _basicModeClass = await BasicModeClass.initialize(_mapboxMap, basic);
+    _logger.info('Mode changed to Basic Map Mode');
+  }
+
+  Future<void> _handleLocSelMode(LocationSelectionMode locSel) async {
+    _mode.ensureMode<LocationSelectionMode>();
+    _locationModeClass = await LocationModeClass.initialize(locSel, _mapboxMap);
+    _logger.info('Mode changed to Location Selection');
+  }
+
+  Future<void> _handleRouteMode(RouteMode routeMode) async {
+    _mode.ensureMode<RouteMode>();
+    _routeModeClass = await RouteModeClass.initialize(routeMode, _mapboxMap);
+    _logger.info('Mode changed to Route mode');
+  }
+
+  Future<void> _handleTrackingMode(TrackingMode tracking) async {
+    _mode.ensureMode<TrackingMode>();
+    _trackingModeClass = await TrackingModeClass.initialize(
+        _mapboxMap, tracking.route, _animationController,
+        waypoints: tracking.waypoints);
+    _logger.info('Mode changed to Tracking Mode');
+  }
+
   Future<void> addRoute(LineString route) async {
     _mode.ensureMode<RouteMode>();
     await _routeModeClass!.addLineString(route, _mapboxMap);
@@ -88,34 +114,8 @@ class MapManager extends ChangeNotifier {
 
   LineString get routeTraversed => _trackingModeClass!.routeTraversed;
 
-  Future<void> _handleRideTrackingMode(TrackingMode tracking) async {
-    _mode.ensureMode<TrackingMode>();
-    _trackingModeClass = await RideTrackingModeClass.initialize(
-        _mapboxMap, tracking.route, _animationController,
-        waypoints: tracking.waypoints);
-    _logger.info('Mode changed to Ride Tracking Mode');
-  }
-
-  Future<void> _handleBasicMode(BasicMapMode basic) async {
-    _mode.ensureMode<BasicMapMode>();
-    _basicModeClass = await BasicModeClass.initialize(_mapboxMap, basic);
-    _logger.info('Mode changed to Basic Map Mode');
-  }
-
-  Future<void> _handleLocSelMode(LocationSelectionMode locSel) async {
-    _mode.ensureMode<LocationSelectionMode>();
-    _locationModeClass = await LocationModeClass.initialize(locSel, _mapboxMap);
-    _logger.info('Mode changed to Location Selection');
-  }
-
   ListValueNotifier? get pointsNotifier => _locationModeClass?.pointsNotifier;
   Point? get lastTapped => _locationModeClass?.lastTapped;
-
-  Future<void> _handleRouteMode(RouteMode routeMode) async {
-    _mode.ensureMode<RouteMode>();
-    _routeModeClass = await RouteModeClass.initialize(routeMode, _mapboxMap);
-    _logger.info('Mode changed to Route mode');
-  }
 
   Future<void> moveCamTo(Point point) async =>
       await moveMapCamTo(_mapboxMap, point);
