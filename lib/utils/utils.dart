a27c71b5dc9fdcb19fd9dc6bd4910dec645648d1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-06-10 19:03:00 +0530

    Add fetchImageFromNetworkImage function to retrieve images from a URL

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,6 +1,34 @@
+import 'dart:async';
+import 'dart:ui' as ui;
+
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 Uint8List addImageFromAsset(ByteData asset) {
   final list = asset.buffer.asUint8List();
   return list;
+}
+
+Future<Uint8List> fetchImageFromNetworkImage(String imageUrl) async {
+  try {
+    final networkImage = NetworkImage(imageUrl);
+    final imageStream = networkImage.resolve(const ImageConfiguration());
+
+    final completer = Completer<ui.Image>();
+    imageStream.addListener(ImageStreamListener((info, _) {
+      completer.complete(info.image);
+    }));
+
+    final image = await completer.future;
+    final byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+
+    if (byteData != null) {
+      return byteData.buffer.asUint8List();
+    } else {
+      throw Exception('Failed to convert image to bytes');
+    }
+  } catch (e) {
+    print('Error fetching image with NetworkImage: $e');
+    rethrow;
+  }
 }
