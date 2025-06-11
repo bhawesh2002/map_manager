13ce9b3a6fb28728bdf398a1d3c12a30cf897f08
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-06-11 07:55:05 +0530

    Specify type for selectedLoc variable in MapAssets class for improved type safety

--- lib/manager/map_assets.dart
+++ lib/manager/map_assets.dart
@@ -1,7 +1,9 @@
+import 'dart:typed_data';
+
 import 'package:map_manager_mapbox/utils/utils.dart';
 
 class MapAssets {
-  static late final selectedLoc;
+  static late final Uint8List selectedLoc;
   static Future<void> init() async {
     selectedLoc = await fetchImageFromNetworkImage(
         "https://github.com/bhawesh2002/map_manager_mapbox/raw/refs/heads/main/assets/selected-loc.png");
