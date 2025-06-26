ceca0c9ecf796cfb26aacb5ec9cb9beecccbefc2
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-06-26 19:12:48 +0530

    Refactor MapAssets to load images from local assets instead of network URLs for improved performance and reliability

--- lib/manager/map_assets.dart
+++ lib/manager/map_assets.dart
@@ -1,14 +1,21 @@
-import 'dart:typed_data';
-
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
 import 'package:map_manager_mapbox/utils/utils.dart';
 
 class MapAssets {
   static late final Uint8List selectedLoc;
   static late final Uint8List personLoc;
   static Future<void> init() async {
-    selectedLoc = await fetchImageFromNetworkImage(
-        "https://github.com/bhawesh2002/map_manager_mapbox/raw/refs/heads/main/assets/selected-loc.png");
-    personLoc = await fetchImageFromNetworkImage(
-        "https://github.com/bhawesh2002/map_manager_mapbox/raw/refs/heads/main/assets/person-loc.png");
+    try {
+      final byteDataPerson = await rootBundle
+          .load('packages/map_manager_mapbox/assets/person-loc.png');
+      final byteDataSelected = await rootBundle
+          .load('packages/map_manager_mapbox/assets/selected-loc.png');
+
+      selectedLoc = addImageFromAsset(byteDataSelected);
+      personLoc = addImageFromAsset(byteDataPerson);
+    } catch (e) {
+      debugPrint("Map Assets.init(): $e");
+    }
   }
 }
