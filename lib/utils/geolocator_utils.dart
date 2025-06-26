fb5d8ca9b33652c4b085cf9b007d3e983bdb8226
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-06-26 21:50:33 +0530

    Add GeolocatorUtils class for location updates management

---
+++ lib/utils/geolocator_utils.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:logging/logging.dart';
+
+class GeolocatorUtils {
+  StreamSubscription? _subscription;
+  static final StreamController<Position> _streamController =
+      StreamController<Position>.broadcast();
+  static Stream<Position> get positionStream => _streamController.stream;
+  static final ValueNotifier<Position?> positionValueNotifier =
+      ValueNotifier(null);
+
+  static Position? get position => positionValueNotifier.value;
+
+  static bool _started = false;
+
+  final Logger _logger = Logger("GeolocatorUtils");
+
+  static void startLocationUpdates() async {
+    final GeolocatorUtils utils = GeolocatorUtils();
+    if (_started == true) return;
+    final lastKnown = await Geolocator.getLastKnownPosition();
+    positionValueNotifier.value = lastKnown;
+    lastKnown != null ? _streamController.sink.add(lastKnown) : null;
+    utils._subscription = Geolocator.getPositionStream().listen((position) {
+      _streamController.sink.add(position);
+      positionValueNotifier.value = position;
+    }, onDone: () {
+      utils._subscription!.cancel();
+    }, onError: (err) {
+      utils._subscription!.cancel();
+    });
+    _started = true;
+    utils._logger.info("Location updates started ");
+  }
+}
