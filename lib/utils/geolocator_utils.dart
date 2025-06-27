f5e93addb72b8bb560b617b31fe0fd2940469ffc
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-06-27 11:36:38 +0530

    Refactor GeolocatorUtils to use LocationUpdate for position updates and improve stream handling

--- lib/utils/geolocator_utils.dart
+++ lib/utils/geolocator_utils.dart
@@ -3,30 +3,30 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:logging/logging.dart';
+import 'package:map_manager_mapbox/map_manager_mapbox.dart';
+import 'package:map_manager_mapbox/utils/geojson_extensions.dart';
 
 class GeolocatorUtils {
   StreamSubscription? _subscription;
   static final StreamController<Position> _streamController =
       StreamController<Position>.broadcast();
   static Stream<Position> get positionStream => _streamController.stream;
-  static final ValueNotifier<Position?> positionValueNotifier =
+  static final ValueNotifier<LocationUpdate?> positionValueNotifier =
       ValueNotifier(null);
 
-  static Position? get position => positionValueNotifier.value;
+  static LocationUpdate? get update => positionValueNotifier.value;
 
-  bool get _started => _subscription != null || _subscription?.isPaused != true;
+  bool get _started => _subscription != null;
 
   final Logger _logger = Logger("GeolocatorUtils");
 
-  static void startLocationUpdates() async {
+  static Future<void> startLocationUpdates() async {
     final GeolocatorUtils utils = GeolocatorUtils();
     if (utils._started == true) return;
-    final lastKnown = await Geolocator.getLastKnownPosition();
-    positionValueNotifier.value = lastKnown;
-    lastKnown != null ? _streamController.sink.add(lastKnown) : null;
     utils._subscription = Geolocator.getPositionStream().listen((position) {
       _streamController.sink.add(position);
-      positionValueNotifier.value = position;
+      positionValueNotifier.value = LocationUpdate(
+          location: position.mapboxPoint, lastUpdated: DateTime.now());
     }, onDone: () {
       utils._subscription!.cancel();
     }, onError: (err) {
