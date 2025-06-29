14315783513cea8d324788186ece9a701499b642
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-06-29 11:59:05 +0530

    Add image dimension decoding and storage in MapAssets class

--- lib/manager/map_assets.dart
+++ lib/manager/map_assets.dart
@@ -1,3 +1,4 @@
+import 'dart:ui' as ui;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:map_manager_mapbox/utils/utils.dart';
@@ -5,6 +6,11 @@ import 'package:map_manager_mapbox/utils/utils.dart';
 class MapAssets {
   static late final Uint8List selectedLoc;
   static late final Uint8List personLoc;
+  static late final int selectedLocWidth;
+  static late final int selectedLocHeight;
+  static late final int personLocWidth;
+  static late final int personLocHeight;
+
   static Future<void> init() async {
     try {
       final byteDataPerson = await rootBundle
@@ -12,10 +18,28 @@ class MapAssets {
       final byteDataSelected = await rootBundle
           .load('packages/map_manager_mapbox/assets/selected-loc.png');
 
+      // Decode images to get dimensions
+      final personImage = await _decodeImageFromByteData(byteDataPerson);
+      final selectedImage = await _decodeImageFromByteData(byteDataSelected);
+
+      // Store dimensions
+      personLocWidth = personImage.width;
+      personLocHeight = personImage.height;
+      selectedLocWidth = selectedImage.width;
+      selectedLocHeight = selectedImage.height;
+
+      // Store processed image data
       selectedLoc = addImageFromAsset(byteDataSelected);
       personLoc = addImageFromAsset(byteDataPerson);
     } catch (e) {
       debugPrint("Map Assets.init(): $e");
     }
+  }
+
+  static Future<ui.Image> _decodeImageFromByteData(ByteData byteData) async {
+    final Uint8List bytes = byteData.buffer.asUint8List();
+    final ui.Codec codec = await ui.instantiateImageCodec(bytes);
+    final ui.FrameInfo frameInfo = await codec.getNextFrame();
+    return frameInfo.image;
   }
 }
