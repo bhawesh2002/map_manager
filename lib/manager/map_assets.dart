3fb29f062fc554c9689b456093555f0d3b58a5af
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-09-11 22:15:30 +0530

    refactor: Simplify asset loading in MapAssets by consolidating logic and improving initialization

--- lib/manager/map_assets.dart
+++ lib/manager/map_assets.dart
@@ -1,46 +1,45 @@
+import 'dart:ui' as ui;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:map_manager/utils/utils.dart';
 
 class MapAssets {
+  static const String _packageName = 'map_manager';
+  static const _base = 'packages/$_packageName/assets/';
+
   static late final MapAsset selectedLoc;
   static late final MapAsset personLoc;
   static late final MapAsset circle;
 
+  static bool _initialized = false;
+
   static Future<void> init() async {
+    if (_initialized) return;
+
     try {
-      final byteDataPerson = await rootBundle.load(
-        'packages/map_manager_mapbox/assets/person-loc.png',
-      );
-      final byteDataSelected = await rootBundle.load(
-        'packages/map_manager_mapbox/assets/selected-loc.png',
-      );
-      final byteDataCircle = await rootBundle.load(
-        'packages/map_manager_mapbox/assets/circle.png',
-      );
+      personLoc = await _loadAsset('person-loc.png');
+      selectedLoc = await _loadAsset('selected-loc.png');
+      circle = await _loadAsset('circle.png');
 
-      final personImage = await decodeImageFromByteData(byteDataPerson);
-      final selectedImage = await decodeImageFromByteData(byteDataSelected);
-      final circleImage = await decodeImageFromByteData(byteDataCircle);
-
-      selectedLoc = MapAsset(
-        asset: addImageFromAsset(byteDataSelected),
-        width: selectedImage.width,
-        height: selectedImage.height,
+      _initialized = true;
+    } catch (e, st) {
+      debugPrint(
+        "MapAssets.init() map_manager/lib/map_assets.dart error: $e\n$st",
       );
-      personLoc = MapAsset(
-        asset: addImageFromAsset(byteDataPerson),
-        width: personImage.width,
-        height: personImage.height,
-      );
-      circle = MapAsset(
-        asset: addImageFromAsset(byteDataCircle),
-        width: circleImage.width,
-        height: circleImage.height,
-      );
-    } catch (e) {
-      debugPrint("Map Assets.init(): $e");
+      rethrow;
     }
+  }
+
+  static Future<MapAsset> _loadAsset(String fileName) async {
+    final byteData = await rootBundle.load('$_base$fileName');
+    final codec = await ui.instantiateImageCodec(byteData.buffer.asUint8List());
+    final frame = await codec.getNextFrame();
+
+    return MapAsset(
+      asset: addImageFromAsset(byteData),
+      width: frame.image.width,
+      height: frame.image.height,
+    );
   }
 }
 
