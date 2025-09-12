8537fddce1329c43abc6f3bd44f15d187f8d4749
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-09-12 18:15:39 +0530

    refactor: Enhance type consistency by updating selectedPointsMap to use Map<String, GeoJSONPoint>

--- lib/manager/data_classes/loc_mode_class.dart
+++ lib/manager/data_classes/loc_mode_class.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/services.dart';
+import 'package:geojson_vi/geojson_vi.dart';
 import 'package:map_manager/manager/map_assets.dart';
 import 'package:map_manager/map_manager.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
@@ -10,44 +11,59 @@ class LocationModeClass implements ModeHandler {
 
   final ManagerLogger _logger = ManagerLogger('LocationModeClass');
 
-  late final PointAnnotationManager _pointAnnotationManager;
-  final List<PointAnnotation> _annotations = [];
-  ListValueNotifier<Point> pointsNotifier = ListValueNotifier<Point>([]);
+  final Map<String, GeoJSONPoint> _selectedPointsMap = {};
+  Map<String, GeoJSONPoint> get selectedPointsMap => _selectedPointsMap;
 
-  List<PointAnnotation> get pointAnnotations => List.unmodifiable(_annotations);
-  List<Point> get selectedPoints => pointsNotifier.value;
-  Point? get lastTapped =>
-      pointsNotifier.value.isNotEmpty ? pointsNotifier.value.last : null;
+  List<GeoJSONPoint> get selectedPoints => _selectedPointsMap.values.toList();
+  List<String> get pointIdentifiers => _selectedPointsMap.keys.toList();
+
+  GeoJSONPoint? get lastTapped => _selectedPointsMap.values.isNotEmpty
+      ? _selectedPointsMap.values.last
+      : null;
 
   static Future<LocationModeClass> initialize(
     LocSelMode mode,
     MapboxMap map,
   ) async {
     LocationModeClass cls = LocationModeClass(mode, map);
-    await cls._initializePointAnnoManager();
+    await cls._addDefaultImage();
     cls._map.setOnMapTapListener(cls._onMapTapCallback);
     await cls._addInitialPointAnnotations();
     return cls;
   }
 
   void _onMapTapCallback(MapContentGestureContext context) async {
-    if (pointAnnotations.length >= mode.maxSelections) {
+    if (selectedPoints.length >= mode.maxSelections) {
       await removeOldestAnnotation();
     }
-    await addPoint(context.point, zoom: true);
+    await addPoint(context.point.toGeojsonPoint(), zoom: true);
   }
 
-  Future<void> _initializePointAnnoManager() async {
-    _pointAnnotationManager = await _map.annotations
-        .createPointAnnotationManager(id: 'pam');
+  Future<void> _addDefaultImage() async {
+    await _map.style.addStyleImage(
+      'def-locsel-img',
+      1.0,
+      MbxImage(
+        width: MapAssets.selectedLoc.width,
+        height: MapAssets.selectedLoc.height,
+        data: MapAssets.selectedLoc.asset,
+      ),
+      true,
+      [],
+      [],
+      null,
+    );
   }
 
   Future<void> _addInitialPointAnnotations() async {
     if (mode.preselected != null && mode.preselected!.isNotEmpty) {
-      for (var pt in mode.preselected!.take(mode.maxSelections).toList()) {
-        await addPoint(pt.toMbPoint());
+      for (var pt
+          in mode.preselected!.entries
+              .toList()
+              .take(mode.maxSelections)
+              .toList()) {
+        await addPoint(pt.value, id: pt.key);
       }
-
       // Zoom to the bounding box of pre-selected locations
       if (mode.preselected!.isNotEmpty) {
         await zoomToBounds();
@@ -55,42 +71,61 @@ class LocationModeClass implements ModeHandler {
     }
   }
 
-  Future<void> addPoint(Point pt, {bool zoom = false, ByteData? asset}) async {
-    final anno = await _pointAnnotationManager.create(
-      PointAnnotationOptions(
-        geometry: pt,
-        image: asset != null
-            ? addImageFromAsset(asset)
-            : MapAssets.selectedLoc.asset,
-        iconOffset: [
-          0,
-          -28, //calculated value. Only compatible with selectedLoc MapAsset. DO NOT MODIFY!
-        ],
+  Future<void> addPoint(
+    GeoJSONPoint point, {
+    String? id,
+    MapAsset? image,
+    bool zoom = false,
+    ByteData? asset,
+  }) async {
+    id = id ?? "pointAt_${selectedPoints.length + 1}";
+    if (image != null) {
+      await _map.style
+          .addSource(
+            GeoJsonSource(id: '$id-src', data: GeoJSONFeature(point).toJSON()),
+          )
+          .then((_) {
+            _selectedPointsMap.putIfAbsent(id!, () => point);
+          });
+
+      await _map.style.addStyleImage(
+        '$id-img',
+        1.0,
+        MbxImage(width: image.width, height: image.height, data: image.asset),
+        true,
+        [],
+        [],
+        null,
+      );
+    }
+    await _map.style.addLayer(
+      SymbolLayer(
+        id: '$id-lyr',
+        sourceId: '$id-src',
+        iconImage: image == null ? 'def-locsel-img' : '$id-img',
       ),
     );
-    _annotations.add(anno);
-    pointsNotifier.add(anno.geometry);
     zoom ? await zoomToBounds() : null;
   }
 
-  Future<void> removeOldestAnnotation() async {
-    final anno = _annotations.removeAt(0);
-    await _pointAnnotationManager.delete(anno);
-    pointsNotifier.remove(anno.geometry);
+  Future<void> removePoint(String identifier) async {
+    if (!_selectedPointsMap.containsKey(identifier)) {
+      throw Exception('Specified Identifier Was Not Found');
+    }
+    _selectedPointsMap.remove(identifier);
+    await _map.style.removeStyleLayer('$identifier-lyr');
+    await _map.style.removeStyleSource('$identifier-src');
   }
 
-  Future<void> removePoint(PointAnnotation annotation) async {
-    if (_annotations.contains(annotation)) {
-      _pointAnnotationManager.delete(annotation);
-      _annotations.remove(annotation);
-      pointsNotifier.remove(annotation.geometry);
-    }
+  Future<void> removeOldestAnnotation() async {
+    final identifier = _selectedPointsMap.keys.last;
+    await removePoint(identifier);
   }
 
   Future<void> clearAllAnnotations() async {
-    await _pointAnnotationManager.deleteAll();
-    _annotations.clear();
-    pointsNotifier.clear();
+    for (var key in pointIdentifiers) {
+      await removePoint(key);
+    }
   }
 
   /// Zooms the map camera to fit all selected points within the viewport.
@@ -119,14 +154,6 @@ class LocationModeClass implements ModeHandler {
     _logger.info("Cleaning Location Mode Data");
     _map.setOnMapTapListener(null);
     await clearAllAnnotations();
-
-    // Remove any existing Point Annotation Managers if exists
-    try {
-      await _map.annotations.removeAnnotationManagerById('pam');
-    } catch (e) {
-      _logger.warning("Error removing point annotation manager: $e");
-    }
-
     _logger.info("Location Mode Data Cleared");
   }
 }
