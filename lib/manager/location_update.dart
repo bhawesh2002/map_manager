d034b7573c3df099d1e973196fa8cc684db254a1
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-09-14 08:31:31 +0530

    refactor: Change location type from GeoJSONPoint to GeoJSONFeature and update equality/hashCode methods

--- lib/manager/location_update.dart
+++ lib/manager/location_update.dart
@@ -1,14 +1,16 @@
 import 'package:geojson_vi/geojson_vi.dart';
 
 class LocationUpdate {
-  final GeoJSONPoint location;
+  final GeoJSONFeature location;
   final DateTime lastUpdated;
 
   LocationUpdate({required this.location, required this.lastUpdated});
 
+  GeoJSONPoint get point => location.geometry as GeoJSONPoint;
+
   factory LocationUpdate.fromJson(Map<String, dynamic> json) {
     return LocationUpdate(
-      location: GeoJSONPoint.fromJSON(json['location']),
+      location: GeoJSONFeature.fromJSON(json['location']),
       lastUpdated: json.containsKey('last_updated')
           ? DateTime.parse(json['last_updated'])
           : DateTime.now(),
@@ -20,14 +22,11 @@ class LocationUpdate {
     if (identical(this, other)) return true;
     if (other is! LocationUpdate) return false;
 
-    return (location.coordinates[1] == other.location.coordinates[1]) &&
-        (location.coordinates[0] == other.location.coordinates[0]);
+    return (point.coordinates[1] == other.point.coordinates[1]) &&
+        (point.coordinates[0] == other.point.coordinates[0]);
   }
 
   @override
-  int get hashCode => Object.hash(
-    location.coordinates[1],
-    location.coordinates[0],
-    lastUpdated,
-  );
+  int get hashCode =>
+      Object.hash(point.coordinates[1], point.coordinates[0], lastUpdated);
 }
